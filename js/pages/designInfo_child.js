f9019f18b917fe5c7d28e1d3bfcad94a3e27a887
huzhongyuan <1141422520@qq.com>
2018-08-07 11:49:21 +0800

    关于雅鸟

---
+++ js/pages/designInfo_child.js
@@ -0,0 +1,18 @@
+window.onload = () => {
+  $('#etalage').etalage({
+    thumb_image_width: 500,
+    thumb_image_height: 334,
+    source_image_width: 1000,
+    source_image_height: 900,
+    zoom_area_width: 500,//放大镜的大小
+    zoom_area_height: 500,//放大镜的高度
+    zoom_area_distance: 5,//大图显示的位置
+    small_thumbs: 4,//略缩图的个数
+    smallthumb_inactive_opacity: 0.3,//没有放大镜部分的透明度
+    smallthumbs_position: 'bottom',//略缩图的位置，本例是在左边从上到下排列，默认是在底部从左到右排列
+    show_icon: false,
+    autoplay: true,//是否自动轮播，默认是true，也就是默认是自动
+    keyboard: false,
+    zoom_easing: true//淡入淡出效果
+});
+}
