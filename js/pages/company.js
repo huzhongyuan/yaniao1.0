0e6f03d59441bb3d4a501b6d19838e3baf1bc360
ylc <2550767776@qq.com>
2018-08-15 17:23:20 +0800

    详情修改

--- js/pages/company.js
+++ js/pages/company.js
@@ -1,3 +1,5 @@
+let list = null;
+
 window.onload = () => {
     // 加载数据
     $.ajax({
@@ -5,15 +7,22 @@ window.onload = () => {
         success: function (res) {
             let themelist = document.getElementsByClassName('themelist')[0];
             let content = document.getElementById('content');
+            list = res.result;
             let title = '';
             for (let i=0; i<res.result.length; i++) {
-                title += '<span>'+res.result[i].name+'</span>';
+                title += '<span onclick="changeTitle('+i+')">'+res.result[i].name+'</span>';
             }
             themelist.innerHTML = title;
-            content.innerHTML = res.result[2].description;
+            content.innerHTML = res.result[0].description;
         },
         error: function (res) {
             console.log(res);
         }
     })
 };
+
+// 点击事件改变
+function changeTitle(i) {
+    let content = document.getElementById('content');
+    content.innerHTML = list[i].description;
+}
--- js/pages/designInfo_child.js
+++ js/pages/designInfo_child.js
@@ -1,20 +1,4 @@
 window.onload = () => {
-    $('#etalage').etalage({
-        thumb_image_width: 500,
-        thumb_image_height: 334,
-        source_image_width: 1000,
-        source_image_height: 900,
-        zoom_area_width: 500,//放大镜的大小
-        zoom_area_height: 500,//放大镜的高度
-        zoom_area_distance: 5,//大图显示的位置
-        small_thumbs: 4,//略缩图的个数
-        smallthumb_inactive_opacity: 0.3,//没有放大镜部分的透明度
-        smallthumbs_position: 'bottom',//略缩图的位置，本例是在左边从上到下排列，默认是在底部从左到右排列
-        show_icon: false,
-        autoplay: true,//是否自动轮播，默认是true，也就是默认是自动
-        keyboard: false,
-        zoom_easing: true//淡入淡出效果
-    });
 
     let id = getQueryVariable('id');
 
@@ -38,4 +22,95 @@ window.onload = () => {
         type.style.display = 'none';
         type.nextElementSibling.style.display = 'none';
     }
+
+    // 加载基本信息
+    $.ajax({
+        url: baseUrl + '/product/getProductDetailById/' + id,
+        success: function (res) {
+            let headertitile = document.getElementsByClassName('headertitile')[0];
+            let headercontetnt = document.getElementsByClassName('headercontetnt')[0];
+            let designInfodes = document.getElementsByClassName('designInfodes')[0];
+            headertitile.innerHTML = res.result[0].ch_name;
+            headercontetnt.innerHTML = res.result[0].sketch;
+            designInfodes.innerHTML = res.result[0].description;
+        },
+        error: function (res) {
+            console.log(res);
+        }
+    });
+    // 得到详情图片
+    $.ajax({
+        url: baseUrl + '/product/getProductPicById/' + id,
+        success: function (res) {
+            let etalage = document.getElementById('etalage');
+            let etalagesm = document.getElementById('etalagesm');
+            let eta = '';
+            let etasm = '';
+            for (let i = 0; i < res.result.length; i++) {
+                eta += '<li>\n' +
+                    '                    <img class="etalage_thumb_image largeimage" src="' + res.result[i].show_url + '"/>\n' +
+                    '                    <img class="etalage_source_image imagesmax" src="' + res.result[i].show_url + '"/>\n' +
+                    '                </li>';
+                etasm += '<li>\n' +
+                    '                        <img class="etalage_source_image minimages" src="' + res.result[i].show_url + '"/>\n' +
+                    '                    </li>';
+            }
+            etalage.innerHTML = eta;
+            etalagesm.innerHTML = etasm;
+            $('#etalage').etalage({
+                thumb_image_width: 500,
+                thumb_image_height: 334,
+                source_image_width: 1000,
+                source_image_height: 900,
+                zoom_area_width: 500,//放大镜的大小
+                zoom_area_height: 500,//放大镜的高度
+                zoom_area_distance: 5,//大图显示的位置
+                small_thumbs: 4,//略缩图的个数
+                smallthumb_inactive_opacity: 0.3,//没有放大镜部分的透明度
+                smallthumbs_position: 'bottom',//略缩图的位置，本例是在左边从上到下排列，默认是在底部从左到右排列
+                show_icon: false,
+                autoplay: true,//是否自动轮播，默认是true，也就是默认是自动
+                keyboard: false,
+                zoom_easing: true//淡入淡出效果
+            });
+        },
+        error: function (res) {
+            console.log(res);
+        }
+    });
+    // 得到主创团队
+    $.ajax({
+        url: baseUrl + '/product/getProductTeamById/' + id,
+        success: function (res) {
+            let team = document.getElementById('team');
+            let html = '';
+            for (let i = 0; i < res.result.length; i++) {
+                html += '<img src="'+res.result[i].show_url+'" alt="">';
+            }
+            team.innerHTML = html;
+        },
+        error: function (res) {
+            console.log(res);
+        }
+    });
+    // 得到评价,风格标签
+    $.ajax({
+        url: baseUrl + '/product/getProductCommentAndTagsById/' + id,
+        success: function (res) {
+            let headerreport = document.getElementsByClassName('headerreport')[0];
+            let xj = document.getElementsByClassName('xj')[0];
+            let xjtext = document.getElementsByClassName('xjtext')[0];
+            xj.innerHTML = '客户评价：'+res.comments.commentScore;
+            xjtext.innerHTML = res.comments.description;
+
+            let tags = '';
+            for (let i = 0; i < res.tags.length; i++) {
+                tags += '<span>'+res.tags[i].name+'</span>';
+            }
+            headerreport.innerHTML = '风格类型：'+tags;
+        },
+        error: function (res) {
+            console.log(res);
+        }
+    });
 };
