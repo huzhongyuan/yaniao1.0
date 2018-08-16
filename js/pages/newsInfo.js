13b4ea0f1fc5059127f332aa37235f7bdbf278a9
ylc <2550767776@qq.com>
2018-08-16 11:45:36 +0800

    新闻详情

--- js/pages/newsInfo.js
+++ js/pages/newsInfo.js
@@ -18,12 +18,35 @@ window.onload = () => {
             let newsInfocontentcon = document.getElementsByClassName('newsInfocontentcon')[0];
             let back = document.getElementsByClassName('back')[0];
             let next = document.getElementsByClassName('next')[0];
+            let result = res.result;
+            let preNext = res.preNext;
 
-            newsInfocontenttitle.innerHTML = res.result.title;
-            newsInfocontenttime.innerHTML = formatDateTime(res.result.pushTime);
-            newsInfocontentcon.innerHTML = res.result.content;
+            newsInfocontenttitle.innerHTML = result.title;
+            newsInfocontenttime.innerHTML = formatDateTime(result.pushTime);
+            newsInfocontentcon.innerHTML = result.content;
 
-
+            if (preNext.length === 2) {
+                back.innerHTML = preNext[0].title;
+                back.onclick = () => {
+                    window.location.href = 'newsInfo.html?id=' + preNext[0].id;
+                };
+                next.innerHTML = preNext[1].title;
+                next.onclick = () => {
+                    window.location.href = 'newsInfo.html?id=' + preNext[1].id;
+                };
+            } else if (preNext.length === 1) {
+                if (preNext[0].is_top>result.isTop||preNext[0].sort>result.sort||preNext[0].id>result.id) {
+                    back.innerHTML = preNext[0].title;
+                    back.onclick = () => {
+                        window.location.href = 'newsInfo.html?id=' + preNext[0].id;
+                    };
+                } else {
+                    next.innerHTML =  preNext[0].title;
+                    next.onclick = () => {
+                        window.location.href = 'newsInfo.html?id=' + preNext[0].id;
+                    };
+                }
+            }
         },
         error: function (res) {
             console.log(res);
