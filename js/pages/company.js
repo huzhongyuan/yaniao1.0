097919461037254fffd6d2e3083b3713d6fb9a83
boy_lin <1183962356@qq.com>
2018-08-20 11:33:58 +0800

    雅鸟软装：

--- js/pages/company.js
+++ js/pages/company.js
@@ -32,10 +32,11 @@ function changeTitle(that,i) {
         $.ajax({
             url: baseUrl + '/staff/getStaffById?id=',
             success: function (res) {
+                console.log(res)
                 let result = res.result;
-                let html = '<div class="teamphoto">\n' ;
+                let html = '<div class="teamphoto">' ;
                 for (let i=0; i<result.length; i++) {
-                    html += '<img onclick="gotoDetail('+result[i].id+')" src="'+result[i].show_url+'" alt="">';
+                    html += '<img onmouseover="mouseover(this);" onmouseout="mouseout(this);" onclick="gotoDetail('+result[i].id+')" data-name="'+result[i].name+'" data-station-name="'+result[i].station_name+'" src="'+result[i].show_url+'" alt="" onerror="imgNotfind(event);">';
                 }
                 html += '</div>';
                 content.innerHTML = html;
@@ -51,4 +52,36 @@ function changeTitle(that,i) {
 
 function gotoDetail(id) {
     window.location.href = 'memberInfo.html?id='+id;
+}
+
+/**
+ * 鼠标移入事件
+ * @param that
+ */
+function mouseover(that) {
+    var w = parseInt($(that).css("width"))*1.5;//li的宽
+    var h = parseInt($(that).css("height"))*1.5;//li的高
+    var imgUrl = $(that).attr("src");
+    /*
+     <p class="showName"></p>
+     <p class="showStation"></p>
+    * */
+    $(".showName").text($(that).attr("data-name")+" "+ $(that).attr("data-station-name"));
+    //$(".showStation").text($(that).attr("data-station-name"));
+    $(".showImage").css({
+        "left":event.clientX,
+        "top":event.clientY,
+        "width":w+"px",
+        "height":h+"px",
+        "background":"url('"+imgUrl+"') no-repeat",
+        "background-size":"100% 100%",
+        "padding":h-24+"px 0px 0px",
+    }).show();
+}
+/**
+ * 鼠标移出事件
+ * @param that
+ */
+function mouseout(that) {
+    $(".showImage").hide()
 }
